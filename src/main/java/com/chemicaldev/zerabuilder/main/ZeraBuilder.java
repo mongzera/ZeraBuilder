43298624dc7d982d3e54fad3f67d9dbbc0b9455a
mongzera <40034319+mongzera@users.noreply.github.com>
2026-02-01 17:13:15 +0800

    Created generalized methods

--- src/main/java/com/chemicaldev/zerabuilder/main/ZeraBuilder.java
+++ src/main/java/com/chemicaldev/zerabuilder/main/ZeraBuilder.java
@@ -1,11 +1,15 @@
 package com.chemicaldev.zerabuilder.main;
 
-import com.chemicaldev.zerabuilder.dsl.SelectDSL;
+import com.chemicaldev.zerabuilder.dsl.*;
 
 public class ZeraBuilder {
     public SQLDialect dialect = SQLDialect.MYSQL;
-    public SelectDSL selectDSL = new SelectDSL(this);
-
+    private SelectDSL selectDSL = new SelectDSL(this);
+    private CreateDSL createDSL = new CreateDSL(this);
+    private UpdateDSL updateDSL = new UpdateDSL(this);
+    private InsertDSL insertDSL = new InsertDSL(this);
+    private DeleteDSL deleteDSL = new DeleteDSL(this);
+    private AlterDSL  alterDSL  = new AlterDSL (this);
 
     public ZeraBuilder(SQLDialect dialect){
         this.dialect = dialect;
@@ -13,5 +17,21 @@ public class ZeraBuilder {
 
     public SelectDSL list (String... cols){
         return selectDSL.list(cols);
+    }
+
+    public CreateDSL makeTable(String tableName){
+        return createDSL.table(tableName);
+    }
+
+    public InsertDSL insertTo(String tableName){
+        return insertDSL.into(tableName);
+    }
+
+    public DeleteDSL deleteFrom(String tableName){
+        return deleteDSL.from(tableName);
+    }
+
+    public AlterDSL alterTable(String tableName){
+        return alterDSL.table(tableName);
     }
 }
