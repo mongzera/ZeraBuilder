022ed1dc8fd480411567a79d17c5015f8e96969f
mongzera <40034319+mongzera@users.noreply.github.com>
2026-02-08 23:39:10 +0800

    made v0.4.2

--- src/main/java/com/chemicaldev/zerabuilder/main/Main.java
+++ src/main/java/com/chemicaldev/zerabuilder/main/Main.java
@@ -1,10 +1,5 @@
 package com.chemicaldev.zerabuilder.main;
 
 public class Main {
-    public static void main(String[] args){
-        ZeraBuilder builder = new ZeraBuilder(SQLDialect.SQLITE);
 
-        // Add Data Types
-        builder.makeTable("users");
-    }
 }
